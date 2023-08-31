c021d886cb1866270137805b5a9e9def9a1975df
joshi233sj <joshi233sj@gmail.com>
2023-08-31 23:12:55 +0200

    Update ted.py

--- scrapy/websc/websc/spiders/ted.py
+++ scrapy/websc/websc/spiders/ted.py
@@ -1,30 +1,55 @@
 import scrapy
 import pandas as pd
-page_limit = False
 
+# Set the page limit flag
+page_limit = True
+
+# Define the number of pages to scrape
 if page_limit:
     pages = 3
 else:
     pages = 100
 
+# Define the Spider class
 class TedSpider(scrapy.Spider):
-    name = "ted"
-    allowed_domains = ['ted.com']
+    name = "ted"  # Name of the spider
+    allowed_domains = ['ted.com']  # Domains that the spider is allowed to crawl
+    # Generate the start URLs based on the number of pages
     start_urls = [f'https://www.ted.com/talks?page={i}' for i in range(1, pages + 1)]
-    
 
+    # The main parsing function
     def parse(self, response):
+        # Extract video titles from the page
         video_titles = [text.strip() for text in response.xpath('//div[@class="media__message"]/h4[2]/a/text()').extract()]
+        
+        # Extract speaker names from the page
         speaker = response.xpath('//div[@class="media__message"]/h4[1]/text()').extract()
+        
+        # Extract dates from the page
         date = [text.strip() for text in response.xpath('//div[@class="meta"]/span/span/text()').extract()]
+        
+        # Extract links to the videos from the page
         links = response.xpath('//div[@class="media__message"]/h4[2]/a/@href').extract()
-        video_links=[]
-        for link in links:
-            video_links.append("https://www.ted.com" + link)
+        
+        # Construct full links for the videos
+        video_links = ["https://www.ted.com" + link for link in links]
+        
+        # Create a dictionary to hold the scraped data
         data_dictionary = {'Title': video_titles,
                            'Speaker': speaker,
                            'Date Posted': date,
                            'Link': video_links}
+        
+        # Create a DataFrame from the data dictionary
         df = pd.DataFrame(data_dictionary)
+        
+        # Sort the DataFrame by title in ascending order
         df = df.sort_values(by=['Title'], ascending=True)
-        df.to_csv('C:/Users/ASUS/Python Folder/data_scrapy.csv',mode='a', index=False)
+        
+        # Append the data to a CSV file
+        df.to_csv('C:/Users/ASUS/Python Folder/data_scrapy.csv', mode='a', index=False)
+
+# Create an instance of the CrawlerProcess and start the Spider
+process = scrapy.crawler.CrawlerProcess()
+process.crawl(TedSpider)
+process.start()
